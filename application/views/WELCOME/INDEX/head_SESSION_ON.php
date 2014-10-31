b3d217a00699d2ec412d6ce6984f9143bf5cad37
Michael Serrato <programador-experto97@hotmail.com>
2014-10-31 03:11:52 -0600

    Developer: Michael Serrato.

--- application/views/WELCOME/INDEX/head_SESSION_ON.php
+++ application/views/WELCOME/INDEX/head_SESSION_ON.php
@@ -112,15 +112,152 @@
                     overlayClickClose: false,
                     onShow: function(_dialog){
                         var content = _dialog.children('.content');
-                        $content = '<label for="nombre">Nombre: </label>'+
-                                    '<div class="input-control text size3 block" data-role="input-control">'+
-                                        '<input type="text" id="nombre" name="nombre" />'+
-                                    '</div>'+
-                                    '<label for="descripcion">Descripción: </label>'+
-                                    '<div class="input-control textarea size4" data-role="input-control">'+
-                                        '<textarea id="descripcion" name="descripcion"></textarea>'+
-                                    '</div>'
+$content = '<table id="form_addItem">'+
+'   <tr>'+
+'       <td style="padding: 6px;">'+
+'           <label for="nombre">Nombre: </label>'+
+'           <div class="input-control text size3" data-role="input-control">'+
+'               <input type="text" id="nombre" name="nombre" />'+
+'           </div>'+
+'       </td>'+
+'       <td style="padding: 8px;">'+
+'           <label for="cantidad">Cantidad: </label>'+
+'           <div class="input-control text size3" data-role="input-control">'+
+'               <input type="number" id="cantidad" name="cantidad" />'+
+'           </div>'+
+'       </td>'+
+'       <td style="padding: 6px;">'+
+'           <label for="precio_total">Costo Total ($): </label>'+
+'           <div class="input-control text size3" data-role="input-control">'+
+'               <input type="text" id="precio_total" name="precio_total" placeholder="0.00" style="text-align: right;" />'+
+'           </div>'+
+'       </td>'+
+'       <td rowspan="2" style="padding: 6px;">'+
+'                <image id="picItem" src="<?=base_url()?>assets/img/show/new-product.jpg" style="width: 220px; height: 180px; margin: auto; display: block;" class="shadow" />'+
+'               <div class="input-control file" data-role="input-control">'+
+'                <input id="input-addImageItem" type="file" tabindex="-1" style="z-index: 1000; width: 100%; height: 100%;"><input type="text" id="__input_file_item__" readonly="" style="z-index: 1; cursor: default;" value="Click Aquí">'+
+'                <button class="btn-file" type="button"></button>'+
+'            </div>'+
+'       </td>'+
+'    </tr>'+
+'    <tr>'+
+'       <td style="padding: 6px;">'+
+'           <label for="descripcion">Descripción: </label>'+
+'           <div class="input-control textarea size3" data-role="input-control">'+
+'               <textarea id="descripcion" name="descripcion"></textarea>'+
+'           </div>'+
+'       </td>'+
+'       <td style="padding: 6px;">'+
+'           <label for="fecha_caducidad">Fecha de Caducidad: </label>'+
+'           <div class="input-control text size3" data-role="input-control">'+
+'               <input type="text" id="fecha_caducidad" name="fecha_caducidad" />'+
+'           </div>'+
+'       </td>'+
+'       <td style="padding: 6px;">'+
+'           <label for="precio_unitario">Precio Unitario ($): </label>'+
+'           <div class="input-control text size3" data-role="input-control">'+
+'               <input type="text" id="precio_unitario" name="precio_unitario" placeholder="0.00" style="text-align: right;" />'+
+'           </div>'+
+'       </td>'+
+'    </tr>'+
+'    <tr>'+
+'       <td style="padding: 6px;">'+
+'           <label for="color">Color: </label>'+
+'           <div class="input-control text size3" data-role="input-control">'+
+'               <input type="text" id="color" name="color" placeholder="Ej. Azul" />'+
+'           </div>'+
+'       </td>'+
+'       <td style="padding: 6px;">'+
+'           <label for="materiales">Materiales </label>'+
+'           <div class="input-control text size3" data-role="input-control">'+
+'               <input type="text" id="materiales" name="materiales" placeholder="Ej. Aluminio" />'+
+'           </div>'+
+'       </td>'+
+'       <td style="padding: 6px;">'+
+'           <label for="accesorios">Accesorios </label>'+
+'           <div class="input-control text size3" data-role="input-control">'+
+'               <input type="text" id="accesorios" name="accesorios" placeholder="Ej: Cargador, Audifonos, Bocina." />'+
+'           </div>'+
+'       </td>'+
+'       <td style="padding: 6px;">'+
+'           <label for="iva">IVA (%): </label>'+
+'           <div class="input-control text size3" data-role="input-control">'+
+'               <input type="text" id="iva" name="iva" style="text-align: right;" placeholder="Ej: 16.00" />'+
+'           </div>'+
+'       </td>'+
+'    </tr>'+
+'    <tr>'+
+'       <td style="padding: 6px;">'+
+'           <label for="talla">Talla: </label>'+
+'           <div class="input-control text size3" data-role="input-control">'+
+'               <input type="text" id="talla" name="talla" style="text-align: right;" placeholder="Ej. 28.5" />'+
+'           </div>'+
+'       </td>'+
+'       <td style="padding: 6px;">'+
+'           <label for="ingredientes">Ingredientes: </label>'+
+'           <div class="input-control text size3" data-role="input-control">'+
+'               <input type="text" id="ingredientes" name="ingredientes" placeholder="Ej. Arroz, Pescado, Elote." />'+
+'           </div>'+
+'       </td>'+
+'       <td style="padding: 6px;">'+
+'           <label for="medida">Medida: </label>'+
+'           <div class="input-control text size3" data-role="input-control">'+
+'               <input type="text" id="medida" name="medida" placeholder="Ej. Arroz, Pescado, Elote." />'+
+'           </div>'+
+'       </td>'+
+'       <td style="padding: 6px;">'+
+'           <label for="precio_final">Precio Final de Venta ($): </label>'+
+'           <div class="input-control text size3" data-role="input-control">'+
+'               <input type="text" style="text-align: right;" id="precio_final" name="precio_final" placeholder="Ej. 789.50" />'+
+'           </div>'+
+'       </td>'+
+'    </tr>'+
+'    <tr>'+
+'       <td style="padding: 6px;">'+
+'           <label for="tela">Tela: </label>'+
+'           <div class="input-control text size3" data-role="input-control">'+
+'               <input type="text" style="text-align: left;" id="tela" name="tela" placeholder="Ej. Seda." />'+
+'           </div>'+
+'       </td>'+
+'       <td style="padding: 6px;">'+
+'           <label for="cargo_extra">Cargo Extra (%): </label>'+
+'           <div class="input-control text size3" data-role="input-control">'+
+'               <input type="text" id="cargo_extra" name="cargo_extra" style="text-align: right;" placeholder="0.00" />'+
+'           </div>'+
+'       </td>'+
+'       <td style="padding: 6px;">'+
+'           <label for="motivos_del_cargo">Motivos del Cargo: </label>'+
+'           <div class="input-control textarea size3" data-role="input-control">'+
+'               <textarea id="motivos_del_cargo" name="motivos_del_cargo"></textarea>'+
+'           </div>'+
+'       </td>'+
+'       <td style="padding: 6px;">'+
+'           <input type="submit" id="submit_item" name="submit_item" value="Guardar Artículo" class="primary large" style="margin:auto; display:block;" />'+
+'       </td>'+
+'    </tr>'+
+'</table>';
+
                         content.html($content);
+                        $(function(){
+                            
+                            function readURL(input) {
+                                if (input.files && input.files[0]) {
+                                    var reader = new FileReader();
+
+                                    reader.onload = function (e) {
+                                        $('#picItem').attr('src',e.target.result);
+                                        $fileURL = $("#input-addImageItem").val();
+                                        $('#__input_file_item__').attr('value',$fileURL);
+                                    }
+
+                                    reader.readAsDataURL(input.files[0]);
+                                }
+                            }
+
+                            $("#input-addImageItem").change(function(){
+                                readURL(this);
+                            });
+                        });
                     }
                 });
             });
